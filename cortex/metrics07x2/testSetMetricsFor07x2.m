6459d4e5fe4f2b0e576184c1c89abc6938613a3f
Manuel Berning <manuel.berning@brain.mpg.de>
2015-07-17 15:26:27 +0200

    added code to generate test classifications for metrics calculation using ImageJ metric script from 2012 ISBI challenge

--- cortex/metrics07x2/testSetMetricsFor07x2.m
+++ cortex/metrics07x2/testSetMetricsFor07x2.m
@@ -39,9 +39,9 @@ cnet.run.actvtClass = @gpuArray;
 toRemove = 2*settings.border' - cnet.randOfConvn; 
 toRemoveOnEachSide = toRemove./2;
 % Constants for normalization
-% minVal = -1.7;
-% maxVal = 1.7;
-for i=1%length(stacks)
+minVal = -1.7;
+maxVal = 1.7;
+for i=2%length(stacks)
     tic;
     load(stacks(i).targetFile);
     % Cut out ROI, normalize to [0 1]
@@ -49,9 +49,9 @@ for i=1%length(stacks)
         1+toRemoveOnEachSide(2):end-toRemoveOnEachSide(2),1+toRemoveOnEachSide(3):end-toRemoveOnEachSide(3));
     classification = gather(onlyFwdPass3D(cnet, normalizeStack(single(rawBig))));
     % Normalize to [0 1]
-%     classification(classification < minVal) = minVal;
-%     classification(classification > maxVal) = maxVal;
-%     classification = (classification - minVal) ./ (maxVal - minVal);
+    classification(classification < minVal) = minVal;
+    classification(classification > maxVal) = maxVal;
+    classification = (classification - minVal) ./ (maxVal - minVal);
     target = single(target);
 %     tifWriteForISBI(single(target), [dataDirectory 'onlineMaterial\extracted\testSet\forFiji\' num2str(stacks(i).taskID, '%.3i') 'original' '.tif']);
 %     tifWriteForISBI(classification, [dataDirectory 'onlineMaterial\extracted\testSet\forFiji\' num2str(stacks(i).taskID, '%.3i') 'proposal' '.tif']);
@@ -90,18 +90,18 @@ classificationU = interp3(X,Y,Z,classification,Xq,Yq,Zq);
 
 % Focused dynamic range (map intervall [minVal maxVal] to [0 nMaxVal] and map outliers to borders 
 minVal = -0.5; maxVal = 0.5;
-nMaxVal = 0.79;
+nDR = 0.79;
 classificationUFDR = classificationU;
 classificationUFDR(classificationUFDR < minVal) = minVal;
 classificationUFDR(classificationUFDR > maxVal) = maxVal;
 classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
-classificationUFDR = classificationUFDR * nMaxVal;
+classificationUFDR = classificationUFDR * nDR;
 
-for modifier = -0.3:0.1:0.3
+for modifier = -0.15:0.01:0.15
     for i=1:size(classificationU,3)
-        thresholdedMarkers(:,:,i) = bwareaopen(classificationU(:,:,i) > 0.7+modifier, 100);
+        thresholdedMarkers(:,:,i) = bwareaopen(classificationU(:,:,i) > 0.5+modifier, 100);
         thresWS(:,:,i) = watershed(imimposemin(-classificationU(:,:,i), thresholdedMarkers(:,:,i)) ,8) > 0;
-        hminMarkers(:,:,i) = bwareaopen(imextendedmin(-classificationU(:,:,i), .4+modifier), 100);
+        hminMarkers(:,:,i) = bwareaopen(imextendedmin(-classificationU(:,:,i), .6+modifier), 100);
         hminWS(:,:,i) = watershed(imimposemin(-classificationU(:,:,i), hminMarkers(:,:,i)) ,8) > 0;
 %         subplot(2,3,1);
 %         imshow(rawBigU(:,:,i));
@@ -119,8 +119,8 @@ for modifier = -0.3:0.1:0.3
 %         imshow(hminWS(:,:,i)*255);
 %         pause(2);
     end
-tifWriteForISBI( 0.9.*single(hminWS), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\hminWS_h' num2str(.4+modifier) '.tif'])
-tifWriteForISBI( 0.9.*single(thresWS), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\thresWS_th' num2str(.7+modifier) '.tif']);
+tifWriteForISBI( 0.9.*single(hminWS), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\hmin\WS_h' num2str(.4+modifier, '%.2f') '.tif'])
+tifWriteForISBI( 0.9.*single(thresWS), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\thres\WS_th' num2str(.7+modifier, '%.2f') '.tif']);
 end
 
 %% Save files
@@ -183,7 +183,8 @@ MIJ.WindowManager.getImage('classification')
 
 for i=1:30; a(:,:,i) = imread('C:\Users\berningm\Desktop\ISBI challenge\train-volume.tif', 'Index', i); end
 for i=1:30; b(:,:,i) = rot90(imread('C:\Users\berningm\Desktop\ISBI challenge\train-labels.tif', 'Index', i)); end
-subplot(1,2,1); imshow(a(:,:,1)); subplot(1,2,2); imshow(b(:,:,1));
+x = 1000; y = 1000; z = 1000;
+raw = readKnossosRoi('Z:\Data\berningm\20150205paper1submission\datasets\2012-09-28_ex145_07x2_corrected\color\1\', '2012-09-28_ex145_07x2_corrected_mag1', [x x+182; y y+182; z z+54]);
 
 figure;
 subplot(2,2,1);
@@ -203,4 +204,268 @@ title('y-z reslice');
 axis off;
 colormap('gray');
 
+figure;
+subplot(2,2,1);
+imagesc(raw(:,:,27));
+daspect([28 28 11.24]);
+title('x-y reslice');
+axis off;
+subplot(2,2,2);
+imagesc(squeeze(raw(:,91,:)));
+daspect([11.24 28 28]);
+title('x-z reslice');
+axis off;
+subplot(2,2,3)
+imagesc(squeeze(raw(91,:,:)));
+daspect([11.24 28 28]);
+title('y-z reslice');
+axis off;
+colormap('gray');
+
+
 figure; hold on; vIn = hist(single(a(b == 0)),256); vOut = hist(single(a(b==255)),256); bar(vIn/sum(vIn(:)),'r'); bar(vOut/sum(vOut(:)), 'b');
+
+%% 11.06.2015 Automate generation of tiff-stacks for Fiji (this is optimization on training)
+% THis was never used, see cell with different modifier above, bad
+% bookkeppoing
+% Load old cortex CNN
+load([dataDirectory filesep 'supplement' filesep 'extracted' filesep 'cortex - CNN20130516T204040_8_3.mat'], 'cnet');
+% Run on Matlab GPU, was jacket GPU before
+cnet.run.actvtClass = @gpuArray;
+
+% Calculate how much of larger FOV of raw data to trim away
+toRemove = 2*settings.border' - cnet.randOfConvn;
+toRemoveOnEachSide = toRemove./2;
+
+% Grids for upsampling
+[X,Y,Z] = meshgrid(1:100,1:100,1:100);
+[Xq,Yq,Zq] = meshgrid(linspace(1,100,512),linspace(1,100,512),1:100);
+
+for i=1:length(stacks)
+    tic;
+    load(stacks(i).targetFile);
+    % Cut out ROI, normalize to [0 1]
+    rawBig = rawBig(1+toRemoveOnEachSide(1):end-toRemoveOnEachSide(1),...
+        1+toRemoveOnEachSide(2):end-toRemoveOnEachSide(2),1+toRemoveOnEachSide(3):end-toRemoveOnEachSide(3));
+    classification = gather(onlyFwdPass3D(cnet, normalizeStack(single(rawBig))));
+    target = single(target);
+    % Upsample target
+    targetU = interp3(X,Y,Z,single(target > 0),Xq,Yq,Zq);
+    % Upsample classification
+    classificationU = interp3(X,Y,Z,classification,Xq,Yq,Zq);
+    
+    % Focused dynamic range (map intervall [minVal maxVal] to [0 nMaxVal] and map outliers to borders
+    minVal = -0.5; maxVal = 0.5;
+    nDR = 0.79;
+    % One could think about whether e.g. sigmoid here is better
+    classificationUFDR = classificationU;
+    classificationUFDR(classificationUFDR < minVal) = minVal;
+    classificationUFDR(classificationUFDR > maxVal) = maxVal;
+    classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
+    classificationUFDR = classificationUFDR * nDR;
+    
+    for modifier = -0.3:0.1:0.3
+        for z=1:size(classificationU,3)
+            thresholdedMarkers(:,:,z) = bwareaopen(classificationU(:,:,z) > 0.7+modifier, 100);
+            thresWS(:,:,z) = watershed(imimposemin(-classificationU(:,:,z), thresholdedMarkers(:,:,i)) ,8) > 0;
+            hminMarkers(:,:,z) = bwareaopen(imextendedmin(-classificationU(:,:,z), .4+modifier), 100);
+            hminWS(:,:,z) = watershed(imimposemin(-classificationU(:,:,z), hminMarkers(:,:,z)) ,8) > 0;
+        end
+        tifWriteForISBI( 0.9.*single(hminWS), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\hmin\WS_h' num2str(.4+modifier) '.tif'])
+        tifWriteForISBI( 0.9.*single(thresWS), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\thres\WS_th' num2str(.7+modifier) '.tif']);
+    end
+    toSubmit = classificationUFDR + 0.105;
+    toSubmit(~hminWS) = 0;
+    tifWriteForISBI( toSubmit, 'Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\finalI4Up.tif');
+    tifWriteForISBI( targetU, 'Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\targetI4Up.tif');
+
+end
+
+%% 14.06.2015 Automate generation of tiff-stacks for Fiji of all test regions
+
+% Load old cortex CNN
+load([dataDirectory filesep 'supplement' filesep 'extracted' filesep 'cortex - CNN20130516T204040_8_3.mat'], 'cnet');
+% Run on Matlab GPU, was jacket GPU before
+cnet.run.actvtClass = @gpuArray;
+
+% Calculate how much of larger FOV of raw data to trim away
+toRemove = 2*settings.border' - cnet.randOfConvn;
+toRemoveOnEachSide = toRemove./2;
+
+% Grids for upsampling
+[X,Y,Z] = meshgrid(1:100,1:100,1:100);
+[Xq,Yq,Zq] = meshgrid(linspace(1,100,512),linspace(1,100,512),1:100);
+
+for i=15:length(stacks)
+    display(num2str(i));
+    tic;
+    load(stacks(i).targetFile);
+    % Cut out ROI, normalize to [0 1]
+    rawBig = rawBig(1+toRemoveOnEachSide(1):end-toRemoveOnEachSide(1),...
+        1+toRemoveOnEachSide(2):end-toRemoveOnEachSide(2),1+toRemoveOnEachSide(3):end-toRemoveOnEachSide(3));
+    classification = gather(onlyFwdPass3D(cnet, normalizeStack(single(rawBig))));
+    target = single(target);
+    % Upsample target
+    targetU = interp3(X,Y,Z,single(target > 0),Xq,Yq,Zq, 'nearest');
+    % Upsample classification
+    classificationU = interp3(X,Y,Z,classification,Xq,Yq,Zq);
+    
+    % Focused dynamic range (map intervall [minVal maxVal] to [nMinVal nDR] and map outliers to borders
+    minVal = 0; maxVal = 1;
+    nDR = 0.89; nMinVal = 0.105;
+    % One could think about whether e.g. sigmoid here is better
+    classificationUFDR = classificationU;
+    classificationUFDR(classificationUFDR < minVal) = minVal;
+    classificationUFDR(classificationUFDR > maxVal) = maxVal;
+    classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
+    classificationUFDR = classificationUFDR * nDR + nMinVal;
+
+    % Use segmentation to generate borders
+    for z=1:size(classificationU,3)
+        hminMarkers(:,:,z) = bwareaopen(imextendedmin(-classificationU(:,:,z), .7), 100);
+        hminWS(:,:,z) = watershed(imimposemin(-classificationU(:,:,z), hminMarkers(:,:,z)) ,8) > 0;
+    end
+    toSubmit=classificationUFDR;
+%     tifWriteForISBI( toSubmit, ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\20150622\c' num2str(i, '%.2i') '.tif']);
+    toSubmit(~hminWS) = 0;
+%     tifWriteForISBI( toSubmit, ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\20150622\w' num2str(i, '%.2i') '.tif']);
+%     tifWriteForISBI( targetU,  ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\20150622\t' num2str(i, '%.2i') '.tif']);
+    toc;
+end
+
+%% Weird shiat for review
+
+    % Focused dynamic range (map intervall [minVal maxVal] to [nMinVal nDR] and map outliers to borders
+    minVal = -1; maxVal = 0;
+    nDR = 1; nMinVal = 0;
+    % One could think about whether e.g. sigmoid here is better
+    classificationUFDR = classificationU;
+    classificationUFDR(classificationUFDR < minVal) = minVal;
+    classificationUFDR(classificationUFDR > maxVal) = maxVal;
+    classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
+    classificationUFDR = classificationUFDR * nDR + nMinVal;
+
+    % Use segmentation to generate borders
+    for z=1:size(classificationU,3)
+        hminMarkers(:,:,z) = bwareaopen(imextendedmin(-classificationU(:,:,z), .7), 100);
+        hminWS(:,:,z) = watershed(imimposemin(-classificationU(:,:,z), hminMarkers(:,:,z)) ,8) > 0;
+    end
+    toSubmit=classificationUFDR;
+    toSubmit(~hminWS) = 0;
+    tifWriteForISBI( toSubmit, ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\new\c' num2str(i, '%.2i') '.tif']);
+
+    %% Weird shiat for review (on original resolution this time)
+
+    clear hminWS hminMarkers;
+    % Focused dynamic range (map intervall [minVal maxVal] to [nMinVal nDR] and map outliers to borders
+    minVal = -1; maxVal = 0;
+    nDR = 1; nMinVal = 0;
+    % One could think about whether e.g. sigmoid here is better
+    classificationUFDR = classification;
+    classificationUFDR(classificationUFDR < minVal) = minVal;
+    classificationUFDR(classificationUFDR > maxVal) = maxVal;
+    classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
+    classificationUFDR = classificationUFDR * nDR + nMinVal;
+
+    % Use segmentation to generate borders
+    for z=1:size(classification,3)
+        hminMarkers(:,:,z) = bwareaopen(imextendedmin(-classification(:,:,z), .7), 100);
+        hminWS(:,:,z) = watershed(imimposemin(-classification(:,:,z), hminMarkers(:,:,z)) ,8) > 0;
+    end
+    toSubmit = classificationUFDR;
+    toSubmit(~hminWS) = 0;
+    tifWriteForISBI( toSubmit, ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\new\cSmall' num2str(i, '%.2i') '.tif']);
+    tifWriteForISBI( single(target > 0.5), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\new\tSmall' num2str(i, '%.2i') '.tif']);
+    
+       %% Weird shiat for review (on original resolution this time)
+       
+    [X,Y,Z] = meshgrid(1:100,1:100,1:100);
+    [Xq,Yq,Zq] = meshgrid(linspace(1,100,round(100/4*11)),linspace(1,100,round(100/4*11)),1:100);
+    targetU = interp3(X,Y,Z,single(target > 0),Xq,Yq,Zq, 'nearest');
+    % Upsample classification
+    classificationU = interp3(X,Y,Z,classification,Xq,Yq,Zq);
+    clear hminWS hminMarkers;
+    % Focused dynamic range (map intervall [minVal maxVal] to [nMinVal nDR] and map outliers to borders
+    minVal = -1; maxVal = 0;
+    nDR = 1; nMinVal = 0;
+    % One could think about whether e.g. sigmoid here is better
+    classificationUFDR = classificationU;
+    classificationUFDR(classificationUFDR < minVal) = minVal;
+    classificationUFDR(classificationUFDR > maxVal) = maxVal;
+    classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
+    classificationUFDR = classificationUFDR * nDR + nMinVal;
+
+    % Use segmentation to generate borders
+    for z=1:size(classificationU,3)
+        hminMarkers(:,:,z) = bwareaopen(imextendedmin(-classificationU(:,:,z), .7), 100);
+        hminWS(:,:,z) = watershed(imimposemin(-classificationU(:,:,z), hminMarkers(:,:,z)) ,8) > 0;
+    end
+    toSubmit = classificationUFDR;
+    toSubmit(~hminWS) = 0;
+    tifWriteForISBI( toSubmit, ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\new\cSameVoxelSize' num2str(i, '%.2i') '.tif']);
+    tifWriteForISBI( single(targetU > 0.5), ['Z:\Data\berningm\20150205paper1submission\onlineMaterial\extracted\testSet\forFiji\automated\testSetISBImetrics\new\tSameVoxelSize' num2str(i, '%.2i') '.tif']);
+     
+%% MORE SHIAT: variation of information on 2-D planes (later 3D)
+
+%% Update 24.06.2015 for visualization of 2 "best" SegEM stacks
+i = 15; % Corresponds to c/t/w numbering in files or Test i-1 in Excel as first is trianing region
+
+% Load old cortex CNN
+load([dataDirectory filesep 'supplement' filesep 'extracted' filesep 'cortex - CNN20130516T204040_8_3.mat'], 'cnet');
+% Run on Matlab GPU, was jacket GPU before
+cnet.run.actvtClass = @gpuArray;
+
+% Calculate how much of larger FOV of raw data to trim away
+toRemove = 2*settings.border' - cnet.randOfConvn;
+toRemoveOnEachSide = toRemove./2;
+
+% Grids for upsampling
+[X,Y,Z] = meshgrid(1:100,1:100,1:100);
+[Xq,Yq,Zq] = meshgrid(linspace(1,100,512),linspace(1,100,512),1:100);
+
+display(num2str(i));
+tic;
+load(stacks(i).targetFile);
+% Cut out ROI, normalize to [0 1]
+rawBig = rawBig(1+toRemoveOnEachSide(1):end-toRemoveOnEachSide(1),...
+    1+toRemoveOnEachSide(2):end-toRemoveOnEachSide(2),1+toRemoveOnEachSide(3):end-toRemoveOnEachSide(3));
+classification = gather(onlyFwdPass3D(cnet, normalizeStack(single(rawBig))));
+target = single(target);
+% Upsample target
+targetU = interp3(X,Y,Z,single(target > 0),Xq,Yq,Zq, 'nearest');
+% Upsample classification
+classificationU = interp3(X,Y,Z,classification,Xq,Yq,Zq);
+rawU = interp3(X,Y,Z, single(rawBig(26:end-25,26:end-25,11:end-10)), Xq, Yq, Zq);
+
+% Focused dynamic range (map intervall [minVal maxVal] to [nMinVal nDR] and map outliers to borders
+minVal = 0; maxVal = 1;
+nDR = 0.89; nMinVal = 0.105;
+% One could think about whether e.g. sigmoid here is better
+classificationUFDR = classificationU;
+classificationUFDR(classificationUFDR < minVal) = minVal;
+classificationUFDR(classificationUFDR > maxVal) = maxVal;
+classificationUFDR = (classificationUFDR - minVal) ./ (maxVal - minVal);
+classificationUFDR = classificationUFDR * nDR + nMinVal;
+
+% Use segmentation to generate borders
+for z=1:size(classificationU,3)
+    hminMarkers(:,:,z) = bwareaopen(imextendedmin(-classificationU(:,:,z), .7), 100);
+    hminWS(:,:,z) = watershed(imimposemin(-classificationU(:,:,z), hminMarkers(:,:,z)) ,8) > 0;
+end
+toSubmit=classificationUFDR;
+toSubmit(~hminWS) = 0;
+toc;
+
+makeMovie(rawU, ['C:\Users\berningm\Desktop\ISBI challenge\' num2str(i) 'raw.avi']);
+makeMovie(toSubmit, ['C:\Users\berningm\Desktop\ISBI challenge\' num2str(i) 'submitted.avi']);
+makeMovie(toSubmit > 0, ['C:\Users\berningm\Desktop\ISBI challenge\' num2str(i) 'thresholded.avi']);
+makeMovie(targetU > 0, ['C:\Users\berningm\Desktop\ISBI challenge\' num2str(i) 'target.avi']);
+
+
+maxVal = max(rawU(:));
+minVal = min(rawU(:));
+rawU = rawU - minVal;
+rawU = rawU ./ (maxVal - minVal);
+
+makeMovie(cat(2, rawU, toSubmit, toSubmit > 0, targetU), ['C:\Users\berningm\Desktop\ISBI challenge\' num2str(i) 'combined.avi']);
+
