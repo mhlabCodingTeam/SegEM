d617e93308ee8c7fe0e3104a18c5e3f85853953a
Manuel Berning <manuel.berning@brain.mpg.de>
2019-08-15 22:45:26 +0200

    update script for SegEM training data to run on gaba and without erosion

--- auxiliaryMethods/KLEEtoCNN/selectFiles.m
+++ auxiliaryMethods/KLEEtoCNN/selectFiles.m
@@ -1,14 +1,30 @@
 function out = selectFiles(files)
-% ALmost obsolete function, for full version see manuelCode repo (only
-% makes sense in combination with braintracing DB @braintracing.de)
 
 for i=1:length(files)
-	param = regexp(files(i).name(1:end-4), '\_', 'split');
-	out(i).dataset = param{1};
-	out(i).taskID = str2double(param{2});
-	out(i).user = param{3};
-    out(i).filename = files(i).name;
+    param = regexp(files(i).name(1:end-4), '\_', 'split');
+    dataset(i) = str2num(param{1});
+    tasktype(i) = str2num(param{2});
+    task(i) = str2num(param{3});
+    user{i} = param{4};
+    upload(i) = str2num(param{5}(1:3));
 end
 
-end	
+if any(dataset ~= 3)
+    warning('Wrong dataset ID included');
+end
+
+if any(tasktype ~= 15)
+    warning('Wrong tasktype ID included');
+end
+
+uniqueTasks = unique(task);
+for i=1:length(uniqueTasks);
+    idxTasks = task == uniqueTasks(i);
+    uploads = upload .* idxTasks;
+    [lastUpload, idxMaxUpload] = max(uploads);
+    out(i).taskID = uniqueTasks(i);
+    out(i).lastUpload = lastUpload;
+    out(i).tracer = user{idxMaxUpload};
+    out(i).filename = files(idxMaxUpload).name;
+end
 
